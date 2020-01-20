be7d0ad312384dd19ff1e9ffb5ab97632865c43e
Luan Henrique Guerra de Gregori <luan.gregori@compasso.com.br>
2020-01-20 15:35:57 -0300

    Backend + gitignore configurados

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const routes = require('./routes');
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/omnistack',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+app.use(express.json());
+app.use(routes);
+console.log('Server running...')
+app.listen(3333);
